e8d7b686d38e90511dccc7cda3e978c5bb39bb3b
pyhuiab <pyhuiab@ust.hk>
2024-09-10 23:51:20 +0800

    Add the 3rd print line

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -7,6 +7,7 @@ public class mainApp1 {
         System.out.println("Program ..starting...");
         System.out.println("1 + 1 ="+2);
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
+        System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
         System.out.println("Program ..Ended …");
     }
